fc9d21ae3a0904f436583cd8d3a4dce62059027f
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-04-07 01:20:27 -0600

    detail

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
 		dots: true,
 		items: 1,
 		nav: true,
-		navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
 		autoplay: true,
 		animateOut: 'fadeOut',
 		animateIn: 'fadeInLeft',
